fa27663320c591aea2991a5896bb378d02f21543
Ahmet KAYMAKCI <ahmetkaymak2207@gmail.com>
2023-01-09 17:19:27 +0300

    feat: add everything

--- contracts/hello-worl.sol
+++ contracts/hello-worl.sol
@@ -1,4 +1,5 @@
 // Specifies the version of Solidity, using semantic versioning.
+//
 // Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
 pragma solidity ^0.8.16;
 
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,37 @@
 require('dotenv').config();
 
 require("@nomiclabs/hardhat-ethers");
-const { API_URL, PRIVATE_KEY } = process.env;
+// const { API_URL, PRIVATE_KEY } = process.env;
 
 /**
 * @type import('hardhat/config').HardhatUserConfig
 */
 module.exports = {
-   solidity: "0.8.16",
-   defaultNetwork: "goerli",
-   networks: {
-      hardhat: {},
-      goerli: {
-         url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: process.env.API_URL,
+      accounts: process.env.PRIVATE_KEY.toString()
+    }
+  },
+  solidity: {
+    version: "0.8.16",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
       }
-   },
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
 }
